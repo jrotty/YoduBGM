fc197dc6eca72db251de207f1d75b9a979f34910
jrotty <bssf@qq.com>
2017-10-16 09:31:30 +0800

    Update prbug.js

--- js/prbug.js
+++ js/prbug.js
@@ -9,11 +9,9 @@ if(autopause==1||znst==1){
 if(otheraudio.paused || otheraudio.length==0){var znst=0;
            yaudio.play();
            ody.className = 'iconfont icon-music';
-           console.info("背景音乐恢复播放");  
 }
 else{var znst=1;
   yaudio.pause();
             ody.className = 'iconfont icon-bofang';
-       console.info("背景音乐智能暂停");  
 }
 }
