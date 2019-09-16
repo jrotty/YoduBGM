874039031f8666b221dd3b9e1f11cc64026deae9
vitoland <admin@liyunbin.com>
2019-09-16 18:21:07 +0800

    增加播放错误跳到下一曲 音量默认为 50%

--- js/player.js
+++ js/player.js
@@ -1,28 +1,46 @@
-function playbtu(){
-var oyd = document.getElementById('ydmc');
-if (yaudio.paused) {
-            yaudio.play();
-           oyd.className = 'iconfont icon-music';
-        } else {
-            yaudio.pause();
-            oyd.className = 'iconfont icon-bofang';
-        }
-}
-function next() {
-var oyd=document.getElementById('ydmc');
-if (a == musicArr.length - 1) {
-            a = 0
-        } else {
-            a = a + 1
-        }
-        var sj = musicArr[a];
-        yaudio.src = sj.mp3;
-        yaudio.play();var autopause=0;
-       oyd.className = 'iconfont icon-music';
+function playbtu() {
+    var oyd = document.getElementById('ydmc');
+    if (yaudio.paused) {
+        yaudio.play();
+        yaudio.volume = 0.5;
+        oyd.className = 'iconfont icon-music';
+    } else {
+        yaudio.pause();
+        yaudio.volume = 0.5;
+        oyd.className = 'iconfont icon-bofang';
+    }
 }
 
-yaudio.addEventListener('ended',
-function() {
+function next() {
+    var oyd = document.getElementById('ydmc');
+    if (a == musicArr.length - 1) {
+        a = 0
+    } else {
+        a = a + 1
+    }
+    var sj = musicArr[a];
+    yaudio.src = sj.mp3;
+    yaudio.play();
+    var autopause = 0;
+    yaudio.volume = 0.5;
+    oyd.className = 'iconfont icon-music';
+}
+
+yaudio.addEventListener('ended', function() {
     next();
-},
-false);
+}, false);
+
+
+yaudio.addEventListener("error", function() {
+    next();
+}, false);
+
+
+/////////////////////////////
+
+var ody = document.getElementById('ydmc');
+if (yaudio.paused) {
+    ody.className = 'iconfont icon-bofang';
+} else {
+    ody.className = 'iconfont icon-music';
+}
