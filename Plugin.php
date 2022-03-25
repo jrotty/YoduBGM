f1a8d31ba6a3314b3616be2aa85019b5c964e1a6
jrotty <bssf@qq.com>
2022-03-25 17:11:59 +0800

    1.7.0
    音乐加载失败自动跳下一首，音乐直链与网易云id支持混填，新增开场音乐设置，随机音乐实现方式由js随机改为php随机播放列表

--- Plugin.php
+++ Plugin.php
@@ -4,7 +4,7 @@
  * 
  * @package YoduBGM
  * @author Jrotty
- * @version 1.6.6
+ * @version 1.7.0
  * @link http://blog.zezeshe.com
  */
 class YoduBGM_Plugin implements Typecho_Plugin_Interface
@@ -23,14 +23,14 @@ while ($n<=200) {
 $d[$n] = $n.'px';$n=$n+5;
 }
 
-    $set2 = new Typecho_Widget_Helper_Form_Element_Select('top', $d, NULL, _t('距离顶部间距'), _t('播放器按钮显示在网页的右上角，这里的设置就是播放器组件距离顶部的间距，默认为65px'));
-    $form->addInput($set2);
+    $set1 = new Typecho_Widget_Helper_Form_Element_Select('top', $d, NULL, _t('距离顶部间距'), _t('播放器按钮显示在网页的右上角，这里的设置就是播放器组件距离顶部的间距，默认为65px'));
+    $form->addInput($set1);
 
-$sxj = new Typecho_Widget_Helper_Form_Element_Radio(
+$set2 = new Typecho_Widget_Helper_Form_Element_Radio(
         'sxj', array('0'=> '隐藏', '1'=> '不隐藏'), 0, '手机端是/否隐藏',
             '');
-        $form->addInput($sxj);
-        $musicList = new Typecho_Widget_Helper_Form_Element_Textarea('musicList', NULL, 
+        $form->addInput($set2);
+        $set3 = new Typecho_Widget_Helper_Form_Element_Textarea('musicList', NULL, 
 '',_t('歌曲列表'), _t('
 <div style="background: #3f51b5;
     color: #fff;
@@ -38,10 +38,13 @@ $sxj = new Typecho_Widget_Helper_Form_Element_Radio(
     margin-top: -0.5em;
 ">填写格式<p><b>直链方式：</b><br>填写歌曲链接地址即可，多首歌曲的话请在两首歌曲之间加换行，千万别多加回车换行。</p>
 <p><b>调用网易云：</b><br>书写网易云歌曲id即可，多首歌曲的话请在两首歌曲id之间加换行，单首歌曲直接写id就行，千万别多加回车换行</p>
-<p><b>注意：</b><br>这两种填写方式不能混合输入，要么只用直链方式，要么只用网易云方式</p>
 </div>
 '));
-        $form->addInput($musicList);
+        $form->addInput($set3);
+    
+        // 开场音乐
+        $set4 = new Typecho_Widget_Helper_Form_Element_Text('kaichang', NULL, NULL, _t('开场音乐'), _t('最开始播放的音乐，因为歌曲列表默认为随机播放，但是如果我们想在开场加个音效旁白，就可以将音乐放在这里，这里支持直链或网易云id，只能填一个音频'));
+        $form->addInput($set4);
     }
     
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
@@ -61,33 +64,31 @@ if(Helper::options()->Plugin('YoduBGM')->top){
  if(empty($musicList)){
        $musicList = "761323";
       }
-      
-      if(strpos($musicList,'//')===false){
-        $musicList = str_replace(PHP_EOL, '.mp3"},{mp3:"https://music.163.com/song/media/outer/url?id=', $musicList);  
-  $musicList = '{mp3:"https://music.163.com/song/media/outer/url?id='.$musicList.'.mp3"}';
-   $musicList = str_replace(array("\r\n", "\r", "\n", " "), "", $musicList);    
-         }else{
-              $musicList = str_replace(PHP_EOL, '"},{mp3:"', $musicList);  
-  $musicList = '{mp3:"'.$musicList.'"}';
-   $musicList = str_replace(array("\r\n", "\r", "\n", " "), "", $musicList);   
-      
-      }
-if(strpos($musicList,',')===false){
-    
-		echo '
-<bgm>			
-<a class="ymusic" onclick="playbtu();" target="_blank"><i id="ydmc"></i></a>
-</bgm>
-             ';
-}else{
-      
-		echo '
-<bgm>			
-<a class="ymusic" onclick="playbtu();" target="_blank"><i id="ydmc"></i></a><a class="ymusic" onclick="next();" id="ydnext" target="_blank"><i class="iconfont icon-you"></i></a>
-</bgm>
-             ';
 
-}
+        $array = explode(PHP_EOL,$musicList);$playList='';$p=1;
+        shuffle($array);
+        if($options->kaichang){$p=2;
+        array_unshift($array,$options->kaichang);
+        }
+        foreach ($array as $value) {
+            $value = trim($value);
+            if (substr($value,0,4) === 'http') {
+                $playList .= '{mp3:"'.$value.'"},';
+            }
+            if (is_numeric($value)) {
+                $playList .= '{mp3:"https://music.163.com/song/media/outer/url?id='.$value.'.mp3"},';
+            }
+            //if (preg_match('/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9a-zA-Z]+$/',$value)) {
+            //    $playList .= '{mp3:"https://www.xxxxxx.com/tool/music/qq.php?id='.$value.'"},';
+            //}
+        }
+
+        if (count($array) <= $p) {
+            echo '<bgm><a class="ymusic" onclick="playbtu();" target="_blank"><i id="ydmc"></i></a></bgm>';
+        } else {
+            echo '<bgm><a class="ymusic" onclick="playbtu();" target="_blank"><i id="ydmc"></i></a><a class="ymusic" onclick="next();" id="ydnext" target="_blank"><i class="iconfont icon-you"></i></a></bgm>';
+        }
+
       
 
 
@@ -95,16 +96,13 @@ if(strpos($musicList,',')===false){
         echo '<script data-no-instant>
 var yaudio = new Audio();
 yaudio.controls = true;
-var musicArr=[
-             '.$musicList.'
-              ];
- 
-/*首次随机播放*/
-var a=parseInt(Math.random()*musicArr.length);
+var musicArr=['.$playList.'];
+var a=0;
 var sj=musicArr[a];
 yaudio.src=sj.mp3;
+yaudio.volume = 0.68;
 </script>'. "\n";
-        echo '<script  src="'.Helper::options()->pluginUrl . '/YoduBGM/js/player.js" data-no-instant></script><script  src="'.Helper::options()->pluginUrl . '/YoduBGM/js/prbug.js"></script>' . "\n";        
+        echo '<script  src="'.Helper::options()->pluginUrl . '/YoduBGM/js/player.js?2022" data-no-instant></script><script  src="'.Helper::options()->pluginUrl . '/YoduBGM/js/prbug.js"></script>' . "\n";        
     }
 
 }
